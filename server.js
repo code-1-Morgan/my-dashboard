2203b1a1fbedaccd874df74cc78738adabdb69e8
mdowning96 <morganodowning@gmail.com>
2022-12-13 10:41:32 -0500

    updated server file

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const passport = require("passport");
 const session = require("express-session");
@@ -7,11 +5,13 @@ const MongoStore = require("connect-mongo")(session);
 const methodOverride = require("method-override");
 const flash = require("express-flash");
 const logger = require("morgan");
-const connectDB = require("./config/database");
 const mainRoutes = require("./routes/main");
 const postRoutes = require("./routes/posts");
 const scheduleRoutes = require("./routes/schedules");
+const express = require('express')
+const mongoose = require('mongoose')
 
+const app = express()
 const PORT = process.env.PORT || 3000
 
 const connectDB = async () => {
@@ -34,7 +34,7 @@ connectDB().then(() => {
     app.listen(PORT, () => {
         console.log("listening for requests");
     })
-})
+  })
 
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
